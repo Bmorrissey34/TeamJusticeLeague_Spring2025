024acef2f66d6ba6ca102ca59a011e1a26a58c06
William Stein <thewillstein@gmail.com>
2025-04-16 17:56:00 -0400

    Setup Item, Consumable & Weapon

--- src/model/Consumable.java
+++ src/model/Consumable.java
@@ -9,11 +9,23 @@ package src.model;
  * @version 1.0
  * Course: ITEC XXXX Spring 2025
  * Written: January 6, 2025
+ * Author: William Stein
  */
 public class Consumable extends Item {
     private int health; // The health value restored by the consumable
 
-    public Consumable() {}
+    /**
+     * Constructor: Consumable
+     * 
+     * @param ID Unique number for consumable
+     * @param name Item consumable
+     * @param description Description of consumable
+     * @param health Amount of health restored upon using consumable
+     */
+    public Consumable(String ID, String name, String description, int health) {
+        super(ID, name, description);
+        this.health = health;
+    }
 
     /**
      * Method: increaseHealth
