0b2eea860687bad7384eef3efa945fda08f6e264
William Stein <thewillstein@gmail.com>
2025-04-19 14:59:16 -0400

    getStrength now returns base strenght plus weapons strength

--- src/model/Player.java
+++ src/model/Player.java
@@ -361,8 +361,23 @@ public class Player {
         this.health = health;
     }
 
+    /**
+     * Method: getStength
+     * 
+     * Returns player's base strength plus the weapon they have in their inventory.
+     * 
+     * @return Int representing strength of player plus weapon strengthn if applicable.
+     * @author William Stein
+     */
     public int getStrength() {
-        return strength;
+        int weaponStrength = 0;
+        for (Item item : inventory.getItems()) {
+            if (item instanceof Weapon) {
+                Weapon weapon = (Weapon) item;
+                weaponStrength = weapon.getStrength();
+            }
+        }
+        return strength + weaponStrength;
     }
 
     public void setStrength(int strength) {
